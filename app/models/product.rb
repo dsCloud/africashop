401ab2199aeb13459434764fb6bb3133f674235e
ds Ears <info@dspheretech.com>
2016-03-10 17:48:33 +0000

    changed the implemented aws storage for images

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "missingimage.png"
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+
 end
