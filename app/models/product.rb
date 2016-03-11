ba30922ddd1ec01b28de213cf41255665d116d45
ds Ears <info@dspheretech.com>
2016-03-11 06:56:30 +0000

    aws setup

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,8 @@ class Product < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "missingimage.png",
   storage: :s3,
   bucket: 'dsafricashop',
-  s3_credentials: "#{Rails.root}/config/S3.yml",
-  path: "/:product/:style/:id/:filename"
+  s3_credentials: "#{Rails.root}/config/S3.yml"
+  #path: "/:product/:style/:id/:filename"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 
