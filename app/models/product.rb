7aa631bd1195503065b0a6f039b847be545b0842
ds Ears <info@dspheretech.com>
2016-03-11 07:53:05 +0000

    resolved all issues on aws storage for images on prodction

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
   require 'aws-sdk'
   has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "missingimage.png",
   storage: :s3,
-  bucket: 'dsafricashop',
+  #bucket: 'dsafricashop',
   s3_credentials: "#{Rails.root}/config/S3.yml"
   #path: "/:product/:style/:id/:filename"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
