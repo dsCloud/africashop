43309624a86e79e357dd685a63c148ede24d6c45
ds Ears <info@dspheretech.com>
2016-03-15 17:13:02 +0000

    controll and redirect URls

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :check_user, only: [:edit, :update, :destroy]
   # GET /products
   # GET /products.json
   def index
@@ -72,4 +73,9 @@ class ProductsController < ApplicationController
     def product_params
       params.require(:product).permit(:name, :description, :price, :category_id, :qtyperunit, :color_id, :image)
     end
+  def check_user
+    if current_user != @product.user
+      redirect_to root_url, alert: "You cannot amend Products that belong ro another seller"
+    end
+  end
 end
