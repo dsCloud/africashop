86f55f5a6d95b30f1144226f09b62fb2821e1351
ds Ears <info@dspheretech.com>
2016-04-02 18:34:04 +0000

    resolving the production aws

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,8 +9,8 @@ class Product < ActiveRecord::Base
         require 'aws-sdk'
         has_attached_file :image, styles: { medium: "200x200", thumb: "100x100>" }, default_url: "missingimage.png",
         storage: :s3,
-        bucket: 'dsafricashop',
-        s3_credentials: "#{Rails.root}/config/application.yml"
+        bucket: 'dsafricashop'
+       # s3_credentials: "#{Rails.root}/config/application.yml"
         #path: "/:product/:style/:id/:filename"
         validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
         #product.image.url => 'https://dsafricashop.s3.amazonaws.com/'
